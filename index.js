6c201785ae0c5c3b639d60fbcf5e12a5d14a7177
dipujafar <dipujafarmr@gmail.com>
2023-11-06 00:12:44 +0600

    added jwt verify middleware

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173/'],
+  credentials:true
+}));
 app.use(express.json());
+
+const verify = async(req, res, next)=>{
+  const token = req?.cookies?.token;
+  if(!token){
+    return res.status(401).send({message: "Unauthorized Access"})
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message : "Unauthorized Access"})
+    }
+    res.user = decoded;
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jafardipu.hwlq4pv.mongodb.net/?retryWrites=true&w=majority`;
@@ -42,7 +60,9 @@ async function run() {
         })
         .send({success : true})
       }
-      catch{}
+      catch{(error)=>{
+        console.log(error)
+      }}
     })
 
     await client.db("admin").command({ ping: 1 });
